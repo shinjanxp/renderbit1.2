bbb2bd17b61c9b0882609eb4fdb790b48176ec99
shinjan <shinjanxp@gmail.com>
2014-07-12 21:10:47 +0530

    created
    renderbit1.2 following 3d rotate schema

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+$(document).ready(function() {
+    $('#mother-wrapper').height(h);
+	
+});
+
+function init_positions(){
+		
+}
