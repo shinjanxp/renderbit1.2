e0efadb1bf25268b61ed0304a280c094d43a022e
shinjan <shinjanxp@gmail.com>
2014-07-14 21:07:02 +0530

    trigo jhat

--- js/functions.js
+++ js/functions.js
@@ -5,12 +5,17 @@ $(document).ready(function() {
     $('#mother-wrapper').height(h);
 	//$('body').height(h);
 	init_positions();
-	alert(tv);
-	alert(tv.length);
 	dropdown();
 });
 var temp;
-var tv=[5,5,65,65],lv=[];
+var tv=[5,5,65,65],lv=[50,50,50,50];
+
+function prefix(t,p,v){
+	$(t).css(p,v);
+	$(t).css("-webkit-"+p,v);
+	$(t).css("-moz-"+p,v);
+	$(t).css("-ms-"+p,v);
+}
 function init_positions(){
 		$('.sub').each(function() {
 			temp=Math.random();
@@ -20,32 +25,38 @@ function init_positions(){
 				$(this).addClass('layer3');
 			else
 				$(this).addClass('layer4');
-			$.merge(tv,[(0.8*h)*Math.random()]);
-			$(this).css('top',(0.8*h)*Math.random()+'px');
+			temp=(0.8*h)*Math.random();
+			$.merge(tv,[temp]);
+			$(this).css('top',temp+'px');
         });
 		
 		$('.philosophy-widget').each(function(index, element) {
-            $(this).css('left',-20 + Math.random()*30+"%");
+			temp=-20 + Math.random()*30;
+			$.merge(lv,[temp]);
+            $(this).css('left',temp+"%");
         });
 		$('.rates-widget').each(function(index, element) {
-            $(this).css('left',-20 + Math.random()*30+"%");
+			temp=-20 + Math.random()*30;
+			$.merge(lv,[temp]);
+            $(this).css('left',temp+"%");
         });
 		$('.portfolio-widget').each(function(index, element) {
-            $(this).css('left',70 + Math.random()*30+"%");
+			temp=70 + Math.random()*30;
+			$.merge(lv,[temp]);
+            $(this).css('left',temp+"%");
         });
 		$('.contact-widget').each(function(index, element) {
-            $(this).css('left',70 + Math.random()*30+"%");
+			temp=70 + Math.random()*30;
+			$.merge(lv,[temp]);
+            $(this).css('left',temp+"%");
         });
 		
 }
 $('#mother-wrapper').mousemove(function(e) {
 	var factor=100;
-    /*$('#philosophy').html(e.pageX);*/
-	$(this).css('transform','rotateY('+ (e.pageX-w/2)/factor +'deg)');
-	$(this).css('webkit-transform','rotateY('+ (e.pageX-w/2)/factor +'deg)');
-	$(this).css('-moz-transform','rotateY('+ (e.pageX-w/2)/factor +'deg)');
-	$(this).css('-ms-transform','rotateY('+ (e.pageX-w/2)/factor +'deg)');
-});
+	prefix($(this),'transform','rotateY('+ (e.pageX-w/2)/factor +'deg)');
+/*	$('#portfolio0 .front p').html(e.pageX+","+e.pageY);
+*/});
 
 function dropdown(){
 	$('.widget').css('margin-top','-100%');	
@@ -110,6 +121,60 @@ $('.widget').mouseleave(function(e) {
    	alert("hi"); 
 });*/
 var stack=["null","null"];
+var head=-1;
 $('.widget').click(function(e) {
-    
+
+    if(head==-1)
+	{
+		arrange($(this));
+	}
+	else if(head==0)
+	{
+		view();
+	}
 });
+
+function arrange(t)
+{
+	var type=t.attr('id');
+	type=type.substring(0,type.length-1);
+	var nodes=$('.'+type+'-widget');
+/*	nodes.each(function(index, element) {
+	alert($(this).attr('id'));
+    });*/
+	temp='#'+type+'0';
+	if(temp=="#philosophy0")
+		$(temp).addClass('active-top-left');
+	if(temp=="#rates0")
+		$(temp).addClass('active-bottom-left');
+	if(temp=="#portfolio0")
+		$(temp).addClass('active-top-right');
+	if(temp=="#contact0")
+		$(temp).addClass('active-bottom-right');
+
+	var x,y,z,index,radius,n,angle;
+	n=$('.'+type+'-widget').length;
+	angle=360/n;
+	radius=Math.min($(temp).width(),$(temp).height());
+	$('.'+type+'-widget').each(function(i, element) {
+		x=0;y=0;z=0;
+       index=$('.widget').index(this);
+		x=radius*Math.cos(Math.PI/180*(90+i*angle));
+		y=radius*Math.sin(Math.PI/180*(90+i*angle));
+		x=x+0.5*w;
+		y=-y+0.5*h;
+/*		alert(x+","+y);*/
+		x=x-0.8*0.5*$(this).width();
+		y=y-0.8*0.5*$(this).height();
+		//alert(x+","+y);
+		x=x-lv[index]*w/100;
+		y=y-tv[index];
+		alert(x+","+y);
+		prefix($(this),"transform","translateX("+x+"px) translateY("+y+"px) translateZ(0px) scale(0.8)");
+		
+    });
+}
+ /*alert($(this).attr('id'));
+ 
+ Math.PI/180*
+ */
